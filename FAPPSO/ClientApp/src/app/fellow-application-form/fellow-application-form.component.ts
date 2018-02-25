839e9d7aa08280509a1f14cbc2d0164dbaad6ee9
Nick Gowdy <nickgowdy87@gmail.com>
2018-02-25 16:25:33 +0000

    Implemented basic expanding/collapsing panels with event handlers with ng-bootstrap.

--- FAPPSO/ClientApp/src/app/fellow-application-form/fellow-application-form.component.ts
+++ FAPPSO/ClientApp/src/app/fellow-application-form/fellow-application-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgbPanelChangeEvent } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'fellow-application-form',
@@ -9,4 +10,17 @@ import { Component } from '@angular/core';
     nextSubmissionDate = '15 December 2017'
     
     followingSubmissionDeadlines = ['7th March 2018','26 Jun 2018','13th Sept 2018']
+
+
+    public beforeChange($event: NgbPanelChangeEvent) {
+       
+
+        if ($event.panelId === 'pnlPersonDetails') {
+            alert("get person details in this event");
+        }
+
+        if ($event.panelId === 'pnlEngineeringCouncilRegistration') {
+            alert("get engineering council registration form in this event");
+        }
+      };
   }
